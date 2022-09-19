d23f2b62f52ae44110c6235ee2705db2cff7e234
anna_tama <anna.tama.roma@gmail.com>
2022-09-19 15:06:27 -0300

    main: reintentar peticiones

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { Product,CreateProductDTO,UpdateProductDTO } from './../models/product.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -20,8 +22,10 @@ export class ProductsService {
       params= params.set('limit',limit)
       params= params.set('offset',offset)
     }
-
-    return this.http.get<Product[]>(this.apiUrl);
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    )
   }
 
   getProduct(id: string){
